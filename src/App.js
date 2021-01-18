52043b4b6093514fafb6791d8dcf2bf3e17e97b6
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2021-01-18 14:04:57 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import Layout from './components/Layout';
-import BurgerBuilder from './containers/BurgerBuilder';
+import React, { Component } from 'react';
 
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-class App extends Component{
+class App extends Component {
   render () {
     return (
-      <div >
+      <div>
         <Layout>
           <BurgerBuilder />
         </Layout>
